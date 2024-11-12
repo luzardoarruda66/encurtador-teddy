f9e880ea8f4581688b8ccd635af24bd2cef99459
Luzardo Arruda <desenvolvedor.luzardo@gmail.com>
2024-11-12 18:54:33 -0300

    added: middleware validation and validor-express

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,22 @@
 import express from "express";
-import { ExpressValidator } from "express-validator";
+import { body } from "express-validator";
 import { signup, login } from "../controllers/authController";
+import { validate } from "../middleware/validationMiddleware";
 
 const router = express.Router();
-router.post("/signup", signup);
-router.post("/login", login);
+router.post(
+  "/signup",
+  [
+    body("username").notEmpty().withMessage("Username is required"),
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("The password min length is 6")
+      .matches(/\d/)
+      .withMessage("The password must contains a number"),
+  ],
+  signup
+);
+router.post("/login",validate, login);
 
 export default router;
--- src/routes/urlRoutes.ts
+++ src/routes/urlRoutes.ts
@@ -6,12 +6,13 @@ import {
   updateUrl,
 } from "../controllers/urlController";
 import { authenticate } from "../middleware/authMiddleware";
+import { validate } from "../middleware/validationMiddleware";
 
 const router = express.Router();
 
-router.get("/list", authenticate, listUserUrls);
-router.post("/shorten", authenticate, shortenUrl);
-router.put("/update/:shortUrl", authenticate, updateUrl);
-router.delete("/delete/:shortUrl", authenticate, deleteUrl);
+router.get("/list", validate, authenticate, listUserUrls);
+router.post("/shorten", validate, authenticate, shortenUrl);
+router.put("/update/:shortUrl", validate, authenticate, updateUrl);
+router.delete("/delete/:shortUrl", validate, authenticate, deleteUrl);
 
 export default router;
